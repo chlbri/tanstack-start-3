ee82fc16b1c282e7fffde154d84b4966ba9fc065
Leonardo Montini <lion.48m@gmail.com>
2024-11-20 17:50:06 +0100

    Project setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { getCount, updateCount } from "../functions";
+
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getCount(),
+});
+
+function Home() {
+  const state = Route.useLoaderData();
+  const router = useRouter();
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({ data: 2 }).then(() => router.invalidate());
+      }}
+    >
+      Add 1 to {state}
+    </button>
+  );
+}
